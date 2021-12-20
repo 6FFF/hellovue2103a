790694e555e13f28df96db4e4362e41aa217095d
6FFF <752157931@qq.com>
2021-12-20 20:15:43 +0800

    第一次提交

---
+++ hello-vue/src/user/list.js
@@ -0,0 +1,14 @@
+//列表 组件
+const listForm = {
+    template:'\
+    <div>\
+    <h2>列表页</h2>\
+    <table border="1" cellpadding="0" cellspacing="0">\
+    <tr><th>姓名</th><th>年龄</th><th>籍贯</th></tr>\
+    <tr><td>房祖名</td><td>210</td><td>北京市</td></tr>\
+    <tr><td>吴奇隆</td><td>210</td><td>北京市</td></tr>\
+    <tr><td>刘德华</td><td>210</td><td>北京市</td></tr>\
+    </table>\
+    </div>\
+    '
+}
